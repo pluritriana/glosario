ffb56a4186b7fa34d48f4aad7129f9f95e7705af
pablo <pcargar144@iescarrillo.es>
2025-12-18 12:28:07 +0100

    no message

--- dist/glosario/server/angular-app-engine-manifest.mjs
+++ dist/glosario/server/angular-app-engine-manifest.mjs
@@ -1,6 +1,6 @@
 
 export default {
-  basePath: '/',
+  basePath: '/glosario',
   supportedLocales: {
   "en-US": ""
 },
--- dist/glosario/server/angular-app-manifest.mjs
+++ dist/glosario/server/angular-app-manifest.mjs
@@ -2,19 +2,19 @@
 export default {
   bootstrap: () => import('./main.server.mjs').then(m => m.default),
   inlineCriticalCss: true,
-  baseHref: '/',
+  baseHref: '/glosario/',
   locale: undefined,
   routes: [
   {
     "renderMode": 2,
-    "route": "/"
+    "route": "/glosario"
   }
 ],
   entryPointToBrowserMapping: undefined,
   assets: {
-    'index.csr.html': {size: 7439, hash: '0de0cecce23791df8495ea5a122b3c901a71843b42cb142aedc044ae486b5b01', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
-    'index.server.html': {size: 958, hash: 'aa473f08ec2e8626123652708a5ef1c817d1cd46f26475b028ff28cd653bec7b', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
-    'index.html': {size: 15856, hash: '76ab2e94c22c210040099a601b6dae56afc6e012f158daa78246d53ff874d368', text: () => import('./assets-chunks/index_html.mjs').then(m => m.default)},
+    'index.csr.html': {size: 7448, hash: '8f6fcffb3831b547b4ebd653cc214902a973fc3396a5bb5662793dc6048a732b', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
+    'index.server.html': {size: 967, hash: 'b03b14b97fadcf6f697566aae43521c769eafec777289d9210c17dfba4c22e06', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
+    'index.html': {size: 15865, hash: 'dd86ccc44814b0dcfac1c54ef4cce9782f046a869729feb81162065ace9a3ba7', text: () => import('./assets-chunks/index_html.mjs').then(m => m.default)},
     'styles-OR5WSFIV.css': {size: 21036, hash: 'tERhyhMUUdg', text: () => import('./assets-chunks/styles-OR5WSFIV_css.mjs').then(m => m.default)}
   },
 };
